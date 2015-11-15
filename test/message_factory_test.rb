b448009517f18fbd6113f59239fda29e5a2d9888
Roman Sova <sova.roman@gmail.com>
2015-11-15 09:33:33 -1000

    tests, added messages folders

--- test/message_factory_test.rb
+++ test/message_factory_test.rb
@@ -3,18 +3,21 @@ require_relative "../lib/ez7gen/message_factory"
 
 class TestMessageFactory < MiniTest::Unit::TestCase
 
-  def test_msh
-    factory = MessageFactory.new
-    hl7 = factory.generate("2.4", "ADT_A01")
-    puts hl7
-    # # assert(hl7 != nil)
-    # refute_nil(hl7)
-  end
+  # def test_msh
+  #   factory = MessageFactory.new
+  #   hl7 = factory.generate("2.4", "ADT_A01")
+  #   puts hl7
+  #   # # assert(hl7 != nil)
+  #   # refute_nil(hl7)
+  # end
 
   def test_msh_vaz_24
-    factory = MessageFactory.new
-    hl7 = factory.generate("vaz2.4", "ADT_A01")
+    ver='vaz2.4'
+    event='ADT_A01'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
     puts hl7
+
     # # assert(hl7 != nil)
     # refute_nil(hl7)
   end
@@ -29,16 +32,20 @@ class TestMessageFactory < MiniTest::Unit::TestCase
 
 # 6	  ADT_A09	  ADT_A10; ADY_A11; ADT_A12 	MSH;EVN;PID;PD1;PV1;PV2;DB1;OBX;DG1
   def test_ADT_A09
-    factory = MessageFactory.new
-    hl7 = factory.generate("2.4", "ADT_A09")
+    ver='2.4'
+    event='ADT_A09'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
     puts hl7
     # # assert(hl7 != nil)
     # refute_nil(hl7)
   end
 # 7	  ADT_A15		                            MSH;EVN;PID;PD1;ROL;PV1;PV2;DB1;OBX;DG1
   def test_ADT_A15
-    factory = MessageFactory.new
-    hl7 = factory.generate("2.4", "ADT_A15")
+    ver='2.4'
+    event='ADT_A15'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
     puts hl7
     # # assert(hl7 != nil)
     # refute_nil(hl7)
@@ -46,8 +53,10 @@ class TestMessageFactory < MiniTest::Unit::TestCase
 
 # 8	  ADT_A16		                            MSH;EVN;PID;PD1;ROL;PV1;PV2;DB1;OBX;DG1;DRG
   def test_ADT_A16
-    factory = MessageFactory.new
-    hl7 = factory.generate("2.4", "ADT_A16")
+    ver='2.4'
+    event='ADT_A16'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
     puts hl7
     # # assert(hl7 != nil)
     # refute_nil(hl7)
@@ -56,8 +65,10 @@ class TestMessageFactory < MiniTest::Unit::TestCase
 # 9 	ADT_A17		                            MSH;EVN;PID;PD1;PV1;PV2;DB1;OBX
   def test_ADT_A17 # Fixed an issue with required repeating segments
     # <MessageStructure name='ADT_A17'  definition='MSH~EVN~PID~[~PD1~]~PV1~[~PV2~]~[~{~DB1~}~]~[~{~OBX~}~]~PID~[~PD1~]~PV1~[~PV2~]~[~{~DB1~}~]~[~{~OBX~}~]' />
-    factory = MessageFactory.new
-    hl7 = factory.generate("2.4", "ADT_A17")
+    ver='2.4'
+    event='ADT_A17'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
     puts hl7
     # # assert(hl7 != nil)
     # refute_nil(hl7)
@@ -65,38 +76,310 @@ class TestMessageFactory < MiniTest::Unit::TestCase
 
 # 10	ADT_A18		                            MSH;EVN;PID;PD1;MGR;PV1
   def test_ADT_A18
-    factory = MessageFactory.new
-    hl7 = factory.generate("2.4", "ADT_A18")
+    ver='2.4'
+    event='ADT_A18'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
     puts hl7
     # # assert(hl7 != nil)
     # refute_nil(hl7)
   end
 
-# 11	ADT_A20		                            MSH;EVN;NPU
-def test_ADT_A20
-  factory = MessageFactory.new
-  hl7 = factory.generate("2.4", "ADT_A20")
-  puts hl7
-  # # assert(hl7 != nil)
-  # refute_nil(hl7)
-end
+  # 11	ADT_A20		                            MSH;EVN;NPU
+  def test_ADT_A20
+    ver='2.4'
+    event='ADT_A20'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
 # 12	ADT_A21	  ADT_A22; ADT_A23; ADT_A25:ADT_A26; ADT_A27; ADT_A29; ADT_A32; ADT_A33 	MSH;EVN;PID;PD1;PV1;PV2;DB1;OBX
+  def test_ADT_A21
+    ver='2.4'
+    event='ADT_A21'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
+  # def test_ADT_A22
+  #   ver='2.4'
+  #   event='ADT_A22'
+  #   hl7 = MessageFactory.new.generate(ver, event)
+  #   File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+  #   puts hl7
+  #   # # assert(hl7 != nil)
+  #   # refute_nil(hl7)
+  # end
+  #
+  # def test_ADT_A23
+  #   ver='2.4'
+  #   event='ADT_A23'
+  #   hl7 = MessageFactory.new.generate(ver, event)
+  #   File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+  #   puts hl7
+  #   # # assert(hl7 != nil)
+  #   # refute_nil(hl7)
+  # end
+  #
+  # def test_ADT_A25
+  #   ver='2.4'
+  #   event='ADT_A25'
+  #   hl7 = MessageFactory.new.generate(ver, event)
+  #   File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+  #   puts hl7
+  #   # # assert(hl7 != nil)
+  #   # refute_nil(hl7)
+  # end
+  #
+  # def test_ADT_A26
+  #   ver='2.4'
+  #   event='ADT_A26'
+  #   hl7 = MessageFactory.new.generate(ver, event)
+  #   File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+  #   puts hl7
+  #   # # assert(hl7 != nil)
+  #   # refute_nil(hl7)
+  # end
+  #
+  # def test_ADT_A27
+  #   ver='2.4'
+  #   event='ADT_A27'
+  #   hl7 = MessageFactory.new.generate(ver, event)
+  #   File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+  #   puts hl7
+  #   # # assert(hl7 != nil)
+  #   # refute_nil(hl7)
+  # end
+  #
+  # def test_ADT_A29
+  #   ver='2.4'
+  #   event='ADT_A29'
+  #   hl7 = MessageFactory.new.generate(ver, event)
+  #   File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+  #   puts hl7
+  #   # # assert(hl7 != nil)
+  #   # refute_nil(hl7)
+  # end
+  #
+  # def test_ADT_A32
+  #   ver='2.4'
+  #   event='ADT_A32'
+  #   hl7 = MessageFactory.new.generate(ver, event)
+  #   File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+  #   puts hl7
+  #   # # assert(hl7 != nil)
+  #   # refute_nil(hl7)
+  # end
+  #
+  # def test_ADT_A33
+  #   ver='2.4'
+  #   event='ADT_A33'
+  #   hl7 = MessageFactory.new.generate(ver, event)
+  #   File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+  #   puts hl7
+  #   # # assert(hl7 != nil)
+  #   # refute_nil(hl7)
+  # end
 # 13	ADT_A24		                            MSH;EVN;PID;PD1;PV1;DB1
+  def test_ADT_A24
+    ver='2.4'
+    event='ADT_A24'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
 # 14	ADT_A30	  ADT_A34; ADT_A35; ADT_A36; ADT_A46; ADT_A47; ADT_A48; ADT_A49 	MSH;EVN;PID;PD1;MRG
+  def test_ADT_A30
+    ver='2.4'
+    event='ADT_A30'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
 # 15	ADT_A37		                            MSH;EVN;PID;PD1;PV1;DB1
+  def test_ADT_A37
+    ver='2.4'
+    event='ADT_A37'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
 # 16	ADT_A38		                            MSH;EVN;PID;PD1;PV1;PV2;DB1;OBX;DG1;DRG
-# 17	ADT_A39	  ADT_A40; ADT_A41; ADT_A42	  MSH;EVN;PID;PD1;MRG;PV1
-# 18	ADT_A43	  ADT_A44 	                  MSH;EVN;PID;PD1;MRG
-# 19	ADT_A45		                            MSH;EVN;PID;PD1;MRG;PV1
-# 20	ADT_A50	  ADT_A51	                    MSH;EVN;PID;PD1;MRG;PV1
-# 21	ADT_A52	  ADT_A53; ADT_A55 	          MSH;EVN;PID;PD1;PV1;PV2
-# 22	ADT_A54		                            MSH;EVN;PID;PD1;ROL;PV1;PV2
-# 23	ADT_A60		                            MSH;EVN;PID;PV1;PV2;IAM
-# 24	ADT_A61	  ADT_A62	                    MSH;EVN;PID;PD1;ROL;PV1;PV2
-# 25	QBP_Q21	  QBP_Q22; QBP_Q23; QBP_Q24	  MSH;QPD;RCP;DSC
-# 26	RSP_K21		                            MSH;MSA;ERR;QAK;QPD;PID;PD1;DSC
-# 28	RSP_K22		                            MSH;MSA;ERR;QAK;QPD;PID;PD1;QRI;DSC
-# 30	RSP_K23		                            MSH;MSA;ERR;QAK;QPD;PID;DSC
-# 32	RSP_K24		                            MSH;MSA;ERR;QAK;QPD;PID;DSC
+  def test_ADT_A38
+    ver='2.4'
+    event='ADT_A38'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
+  # 17	ADT_A39	  ADT_A40; ADT_A41; ADT_A42	  MSH;EVN;PID;PD1;MRG;PV1
+  def test_ADT_A39
+    #failed
+    ver='2.4'
+    event='ADT_A39'
+    # <MessageStructure name='ADT_A39' definition='MSH~EVN~{~PID~[~PD1~]~MRG~[~PV1~]~}' />
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
+  # 18	ADT_A43	  ADT_A44 	                  MSH;EVN;PID;PD1;MRG
+  def test_ADT_A43
+    #failed
+    ver='2.4'
+    event='ADT_A43'
+    # <MessageStructure name='ADT_A43' definition='MSH~EVN~{~PID~[~PD1~]~MRG~}' />
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
+  # 19	ADT_A45		                            MSH;EVN;PID;PD1;MRG;PV1
+  def test_ADT_A45
+    #failed
+    ver='2.4'
+    event='ADT_A45'
+    # <MessageStructure name='ADT_A45' definition='MSH~EVN~PID~[~PD1~]~{~MRG~PV1~}' />
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
+  # 20	ADT_A50	  ADT_A51	                    MSH;EVN;PID;PD1;MRG;PV1
+  def test_ADT_A50
+    ver='2.4'
+    event='ADT_A50'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
+  # 21	ADT_A52	  ADT_A53; ADT_A55 	          MSH;EVN;PID;PD1;PV1;PV2
+  def test_ADT_A52
+    ver='2.4'
+    event='ADT_A52'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
+  # 22	ADT_A54		                            MSH;EVN;PID;PD1;ROL;PV1;PV2
+  def test_ADT_A54
+    ver='2.4'
+    event='ADT_A54'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
+  # 23	ADT_A60		                            MSH;EVN;PID;PV1;PV2;IAM
+  def test_ADT_A60
+    ver='2.4'
+    event='ADT_A60'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
+  # 24	ADT_A61	  ADT_A62	                    MSH;EVN;PID;PD1;ROL;PV1;PV2
+  def test_ADT_A61
+    ver='2.4'
+    event='ADT_A61'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
+  # 25	QBP_Q21	  QBP_Q22; QBP_Q23; QBP_Q24	  MSH;QPD;RCP;DSC
+  def test_QBP_Q21
+    ver='2.4'
+    event='QBP_Q21'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
+  # 26	RSP_K21		                            MSH;MSA;ERR;QAK;QPD;PID;PD1;DSC
+  def test_RSP_K21
+    #failed
+    ver='2.4'
+    event='RSP_K21'
+    # <MessageStructure name='RSP_K21' definition='MSH~MSA~[~ERR~]~QAK~QPD~[~PID~[~PD1~]~]~[~DSC~]' />
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
+  # 28	RSP_K22		                            MSH;MSA;ERR;QAK;QPD;PID;PD1;QRI;DSC
+  def test_RSP_K22
+    #failed
+    ver='2.4'
+    event='RSP_K22'
+    # <MessageStructure name='RSP_K22' definition='MSH~MSA~[~ERR~]~QAK~QPD~{~[~PID~[~PD1~]~[~QRI~]~]~}~[~DSC~]' />
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
+  # 30	RSP_K23		                            MSH;MSA;ERR;QAK;QPD;PID;DSC
+  def test_RSP_K23
+    ver='2.4'
+    event='RSP_K23'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
+
+  # 32	RSP_K24		                            MSH;MSA;ERR;QAK;QPD;PID;DSC
+  def test_RSP_K24
+    ver='2.4'
+    event='RSP_K24'
+    hl7 = MessageFactory.new.generate(ver, event)
+    File.open("../msg-samples/#{ver}/#{event}.txt", 'a') { |f| f.write(hl7); f.write("\n\n") }
+    puts hl7
+    # # assert(hl7 != nil)
+    # refute_nil(hl7)
+  end
 
 end
